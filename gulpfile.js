65ba6b427c26da80969de4edbd928f361b037f24
alexcu21 <alexcu21@live.com>
2019-01-07 19:36:13 -0600

    responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,146 @@
+/*global require */
+/*jslint node:true */
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var autoprefixer = require('gulp-autoprefixer');
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cp = require("child_process");
+const cssnano = require("cssnano");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+//const webpack = require("webpack");
+//const webpackconfig = require("./webpack.config.js");
+//const webpackstream = require("webpack-stream");
 
-gulp.task('sass', function() {
-  gulp.src('scss/estilos.scss')
-    .pipe(autoprefixer()
-    )
-    .pipe(sass({
-      includePaths: ['scss']
-    }))
-    .pipe(gulp.dest('app/css'));
-});
-
-// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
-gulp.task('serve', ['sass'], function() {
-  browserSync.init(["app/css/*.css", "app/js/*.js", "app/*.html"], {
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
     server: {
-      baseDir: 'app'
-    }
+      baseDir: "./app/"
+    },
+    port: 3000
   });
+  done();
+}
 
-});
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
-gulp.task('watch', ['sass', 'serve'], function() {
-  gulp.watch(["scss/*.scss"], ['sass']);
-});
+// Clean assets
+function clean() {
+  return del(["./app/assets/"]);
+}
+
+// Optimize Images
+function images() {
+  return gulp
+    .src("./app/images/**/*")
+    .pipe(newer("./app/images"))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest("./app/images"));
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./app/css/"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./app/css/"))
+    .pipe(browsersync.stream());
+}
+
+function html(){
+  return gulp
+   .src("./app/*.html")
+   .pipe(plumber())
+   .pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+  return gulp
+    .src(["./js/**/*", "./gulpfile.js"])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return gulp
+      .src(["./js/**/*"])
+      .pipe(plumber())
+      .pipe(webpackstream(webpackconfig, webpack))
+      // folder only, filename is specified in webpack config
+      .pipe(gulp.dest("./app/js/"))
+      .pipe(browsersync.stream());
+}
+
+// Jekyll
+function jekyll() {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./scss/**/*", css);
+  gulp.watch("./js/**/*", gulp.series(scriptsLint, scripts));
+  gulp.watch(
+    [
+      "./_includes/**/*",
+      "./_layouts/**/*",
+      "./_pages/**/*",
+      "./_posts/**/*",
+      "./_projects/**/*"
+    ],
+    gulp.series(jekyll, browserSyncReload)
+  );
+  gulp.watch("./img/**/*", images);
+  gulp.watch("./app/*html", html);
+}
+
+// Tasks
+gulp.task("images", images);
+gulp.task("css", css);
+gulp.task("js", gulp.series(scriptsLint, scripts));
+gulp.task("jekyll", jekyll);
+gulp.task("clean", clean);
+
+// build
+gulp.task(
+  "build",
+  gulp.series(clean, gulp.parallel(css, images, jekyll, "js"))
+);
+
+// watch
+gulp.task("watch", gulp.parallel(watchFiles, browserSync));
